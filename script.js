a19eb7a94d5bec35baf83d0aad88a0876461d0ac
adelunaExylys <andrea@exylys.mx>
2025-06-02 20:33:27 -0600

    Initial commit

---
+++ script.js
@@ -0,0 +1,5 @@
+// document.getElementById("parrafo3").innerHTML="Hola, estoy accediendo a JS 2"
+
+window.onload = function() {
+    document.getElementById("parrafo3").innerHTML="Hola, estoy accediendo a JS 3"
+}
